f7995ae875809c3a3fc8c1e2fd86652ad5a50781
Lili Rodriguez <lilimrod@gmail.com>
2020-03-08 05:20:30 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,13 +5,13 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  difference = num2 - num1
+  difference = num1 - num2
   puts difference
   difference
 end
 
 def division(num1, num2)
-  quotient = num2 / num1
+  quotient = num1 / num2
   puts quotient
   quotient
 end
@@ -23,7 +23,7 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  remainder = num2 % num1
+  remainder = num1 % num2
   puts remainder
   remainder
 end
